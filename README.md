7ec6deef7c3d09c0835238a3307c664d441d4cb7
z0x0z <gopikrishna94c@icloud>
2020-09-28 19:08:45 +0530

    first

---
+++ README.md
@@ -0,0 +1,28 @@
+# Mac_Os Setup
+
+This is my Macbook setup for Pentesting and Ethical Hacking, which makes my work a lot much easier. Hence thought of sharing it to the public. 
+
+### Download and Install iTerm2 
+
+`https://www.iterm2.com/downloads.html`
+
+### Download and Install Docker Desktop for Mac
+
+`https://hub.docker.com/editions/community/docker-ce-desktop-mac`
+
+### Kali Linux Docker image
+
+Pull my customised Kali linux image from my docker hub account
+
+`https://hub.docker.com/r/z0x0z/kali_linux_z0x0z`
+
+### Favourite Tools
+
+Install my fav mac_os tools which comes handy in our day 2 day operations
+
+`chmod 700 brew.sh`
+
+`/brew.sh`
+
+![](Screenshots/im.jpeg)
+![](Screenshots/hh)
---
+++ brew.sh
@@ -0,0 +1,51 @@
+
+#!/bin/sh
+
+
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew analytics off
+brew install telnet
+brew install jq
+brew install go
+brew install ngrok
+brew install awscli
+brew install bat
+brew install httpd
+brew install socat
+brew install neofetch
+brew install lolcat
+brew install unfurl
+brew install tmux
+brew install htop
+brew install exiftool
+brew install nikto
+brew install httpie
+brew install ssh-copy-id
+brew install autossh
+brew install git
+brew install git-extras
+brew install git-secret
+brew install git-secrets
+brew install highlight
+brew install wget
+brew install wrk
+brew install tmate
+brew install vim
+brew install colorsvn
+brew install colortail
+brew install crash
+brew install cryptopp
+brew install gnupg
+brew install gotty
+brew install gradle
+brew install graphviz
+
+
+
+
+
+
+
+
